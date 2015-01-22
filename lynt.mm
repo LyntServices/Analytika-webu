a643c97eae45308100fef85c104bb6129eb4da47
Vladimir Smitka <vladimir.smitka@lynt.cz>
2015-01-22 19:46:31 +0100

    Soubory analyzy

---
+++ lynt.mm
@@ -0,0 +1,140 @@
+<map version="1.0.1">
+<node ID="http://lynt.cz/" TEXT="http://lynt.cz/">
+<node ID="kontakty" TEXT="kontakty">
+<node ID="blog/report-z-analyzy-klicovych-slov" TEXT="blog/report-z-analyzy-klicovych-slov">
+</node>
+<node ID="blog/rozdeleni-vyrazu-do-podskupin-dokonceni-analyzy" TEXT="blog/rozdeleni-vyrazu-do-podskupin-dokonceni-analyzy">
+</node>
+</node>
+<node ID="infrastruktura" TEXT="infrastruktura">
+<node ID="infrastruktura/linux-servery" TEXT="infrastruktura/linux-servery">
+</node>
+<node ID="infrastruktura/licence-audity" TEXT="infrastruktura/licence-audity">
+</node>
+<node ID="infrastruktura/zakladni-infrastruktura" TEXT="infrastruktura/zakladni-infrastruktura">
+</node>
+<node ID="infrastruktura/komunikace" TEXT="infrastruktura/komunikace">
+<node ID="blog/predstaveni-ms-sharepoint" TEXT="blog/predstaveni-ms-sharepoint">
+</node>
+</node>
+<node ID="infrastruktura/zabezpeceni" TEXT="infrastruktura/zabezpeceni">
+</node>
+<node ID="infrastruktura/dodavky-vyvoj-hw-sw" TEXT="infrastruktura/dodavky-vyvoj-hw-sw">
+</node>
+<node ID="infrastruktura/microsoft-reseni" TEXT="infrastruktura/microsoft-reseni">
+</node>
+<node ID="infrastruktura/vzdaleny-pristup" TEXT="infrastruktura/vzdaleny-pristup">
+</node>
+</node>
+<node ID="web" TEXT="web">
+<node ID="web/e-shop" TEXT="web/e-shop">
+</node>
+<node ID="web/mobilni-weby" TEXT="web/mobilni-weby">
+</node>
+<node ID="web/wordpress" TEXT="web/wordpress">
+</node>
+<node ID="web/informacni-systemy" TEXT="web/informacni-systemy">
+</node>
+<node ID="web/optimalizace-webu" TEXT="web/optimalizace-webu">
+</node>
+<node ID="web/prezentace" TEXT="web/prezentace">
+</node>
+</node>
+<node ID="marketing" TEXT="marketing">
+<node ID="marketing/seo-optimalizace" TEXT="marketing/seo-optimalizace">
+<node ID="blog/jaky-byl-marketing-festival-2014" TEXT="blog/jaky-byl-marketing-festival-2014">
+</node>
+<node ID="marketing/seo-analyza-webu-audit" TEXT="marketing/seo-analyza-webu-audit">
+</node>
+</node>
+<node ID="marketing/budovani-znacky" TEXT="marketing/budovani-znacky">
+</node>
+<node ID="marketing/bannery-remarketing" TEXT="marketing/bannery-remarketing">
+</node>
+<node ID="marketing/proklikove-kampane" TEXT="marketing/proklikove-kampane">
+</node>
+<node ID="marketing/socialni-site" TEXT="marketing/socialni-site">
+</node>
+<node ID="marketing/marketingove-analyzy" TEXT="marketing/marketingove-analyzy">
+</node>
+</node>
+<node ID="blog" TEXT="blog">
+<node ID="blog/tag/capistrano" TEXT="blog/tag/capistrano">
+</node>
+<node ID="blog/tag/open-refine" TEXT="blog/tag/open-refine">
+</node>
+<node ID="blog/cistime-a-tagujeme-data-pomoci-open-refine" TEXT="blog/cistime-a-tagujeme-data-pomoci-open-refine">
+</node>
+<node ID="blog/capistrano-3-automatizovany-deployment-nette-aplikaci" TEXT="blog/capistrano-3-automatizovany-deployment-nette-aplikaci">
+</node>
+<node ID="blog/kompletni-online-marketingova-strategie-krok-po-kroku" TEXT="blog/kompletni-online-marketingova-strategie-krok-po-kroku">
+</node>
+<node ID="blog?paginator-page=3" TEXT="blog?paginator-page=3">
+<node ID="blog/tag/asa" TEXT="blog/tag/asa">
+</node>
+<node ID="blog/predstaveni-cisco-asa" TEXT="blog/predstaveni-cisco-asa">
+</node>
+<node ID="blog/tag/firewall" TEXT="blog/tag/firewall">
+</node>
+</node>
+<node ID="blog/tag/bezpecnost" TEXT="blog/tag/bezpecnost">
+<node ID="blog/ceske-weby-a-wordpress" TEXT="blog/ceske-weby-a-wordpress">
+</node>
+</node>
+<node ID="blog/tag/nastroje" TEXT="blog/tag/nastroje">
+</node>
+<node ID="blog/tag/deployment" TEXT="blog/tag/deployment">
+</node>
+<node ID="blog/tag/wordpress" TEXT="blog/tag/wordpress">
+</node>
+<node ID="blog/tag/analyza-klicovych-slov" TEXT="blog/tag/analyza-klicovych-slov">
+</node>
+<node ID="blog/detekce-nat-v-podnikove-siti" TEXT="blog/detekce-nat-v-podnikove-siti">
+</node>
+<node ID="blog/tag/automatizace" TEXT="blog/tag/automatizace">
+</node>
+<node ID="blog/ekosystem-ruby-prehled-nastroju" TEXT="blog/ekosystem-ruby-prehled-nastroju">
+</node>
+<node ID="blog/tag/online-strategie" TEXT="blog/tag/online-strategie">
+</node>
+<node ID="blog/vytvarime-prehledy-skupin-pomoci-excelu" TEXT="blog/vytvarime-prehledy-skupin-pomoci-excelu">
+</node>
+<node ID="blog/tag/excel" TEXT="blog/tag/excel">
+</node>
+<node ID="blog/jaka-byla-4-wordpress-konference" TEXT="blog/jaka-byla-4-wordpress-konference">
+</node>
+<node ID="blog/tag/cisco" TEXT="blog/tag/cisco">
+</node>
+<node ID="blog/tag/ruby" TEXT="blog/tag/ruby">
+</node>
+<node ID="blog?paginator-page=2" TEXT="blog?paginator-page=2">
+<node ID="blog/instalace-centos-7" TEXT="blog/instalace-centos-7">
+</node>
+<node ID="blog/tag/outlook" TEXT="blog/tag/outlook">
+</node>
+<node ID="blog/tag/slozka" TEXT="blog/tag/slozka">
+</node>
+<node ID="blog/jaka-byla-3-wordpress-konference" TEXT="blog/jaka-byla-3-wordpress-konference">
+</node>
+<node ID="blog/jake-bylo-webexpo-2014" TEXT="blog/jake-bylo-webexpo-2014">
+</node>
+<node ID="blog/jak-najit-upgrade-na-windows-8-1" TEXT="blog/jak-najit-upgrade-na-windows-8-1">
+</node>
+<node ID="blog/wordpress-contact-form-7-zmeny-v-integraci-od-verze-3-9" TEXT="blog/wordpress-contact-form-7-zmeny-v-integraci-od-verze-3-9">
+</node>
+<node ID="blog/checklist-wordpress-migrace-do-produkce" TEXT="blog/checklist-wordpress-migrace-do-produkce">
+</node>
+<node ID="blog/chybejici-panel-slozek-v-outlooku-2013" TEXT="blog/chybejici-panel-slozek-v-outlooku-2013">
+</node>
+<node ID="blog/tag/zmizela" TEXT="blog/tag/zmizela">
+</node>
+</node>
+<node ID="blog/tag/akce" TEXT="blog/tag/akce">
+</node>
+</node>
+<node ID="infrastruktura/konzultace-skoleni" TEXT="infrastruktura/konzultace-skoleni">
+</node>
+<node ID="blog/mikroftp" TEXT="blog/mikroftp">
+</node>
+</node>
+</map>
---
+++ ukazka.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<node ID="ID_289617429" TEXT="A">
+	<node ID="ID_910564107" TEXT="B"/>
+	<node ID="ID_139054196" TEXT="C"/>
+</node>
+</map>
